26a7825f6c91eedfa7ddf8322785d8c28a7f5e2d
Ali AB <aliabdev07@gmail.com>
2024-09-12 02:46:44 +0100

    Update models.py
    Add class Categorie and Book

--- lmsdashboard/lms_app/models.py
+++ lmsdashboard/lms_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Categorie(models.Model):
+    name = models.CharField(max_length=20)
+    
+class Book(models.Model):
+    status_book = [
+        ('available', 'available'),
+        ('rental', 'rental'),
+        ('sold', 'sold'),
+    ]
+
+    title = models.CharField(max_length=20)
+    author = models.CharField(max_length=20)
+    book_img = models.ImageField(upload_to='photo', null=True, blank=True)
+    author_img = models.ImageField(upload_to='photo', null=True, blank=True)
+    pages = models.IntegerField(null=True, blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    rental_bookprice = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    retal_period = models.IntegerField(null=True, blank=True)
+    active = models.BooleanField(default=True)
+    status = models.CharField(max_length=50, choices=status_book, null=True, blank=True)
+    categorie = models.ForeignKey(Categorie, on_delete=models.PROTECT, null=True, blank=True)
+    
