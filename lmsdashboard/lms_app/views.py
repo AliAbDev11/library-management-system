8f11d81b643ffbcbd448ce6a71a660d785120caa
Ali AB <aliabdev07@gmail.com>
2024-09-12 02:47:35 +0100

    Update views.py
    add a def for  index and books

--- lmsdashboard/lms_app/views.py
+++ lmsdashboard/lms_app/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def base(request):
     return render(request, 'base.html')
+
+def index(request):
+    return render(request, 'pages/index.html')
+
+def books(request):
+    return render(request, 'pages/books.html')
