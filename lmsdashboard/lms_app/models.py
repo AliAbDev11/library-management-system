0053cfd00a4dddb7fe9772e7d410f883ba6e1a55
Ali AB <aliabdev07@gmail.com>
2024-09-17 03:02:32 +0100

    update models.py
    - add field total_rental

--- lmsdashboard/lms_app/models.py
+++ lmsdashboard/lms_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Categorie(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=20)
     def __str__(self):
         return self.name
@@ -21,11 +21,12 @@ class Book(models.Model):
     author_img = models.ImageField(upload_to='photo', null=True, blank=True)
     pages = models.IntegerField(null=True, blank=True)
     price = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    total_rental = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
     rental_bookprice = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
     retal_period = models.IntegerField(null=True, blank=True)
     active = models.BooleanField(default=True)
     status = models.CharField(max_length=50, choices=status_book, null=True, blank=True)
-    category = models.ForeignKey(Categorie, on_delete=models.PROTECT, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, blank=True)
 
     def __str__(self):
         return self.title
