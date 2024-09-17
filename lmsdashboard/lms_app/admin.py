b8e5beda6ce1329b93cfb26c78d7d8aa07de141b
Ali AB <aliabdev07@gmail.com>
2024-09-17 03:00:29 +0100

    update admin.py
    - change Categorie to Category

--- lmsdashboard/lms_app/admin.py
+++ lmsdashboard/lms_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Book, Categorie
+from .models import Book, Category
 
 # Register your models here.
 admin.site.register(Book)
-admin.site.register(Categorie)
+admin.site.register(Category)
