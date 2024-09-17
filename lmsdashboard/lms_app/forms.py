3379a9245ae8767ad40908b7481b477fff9509ec
Ali AB <aliabdev07@gmail.com>
2024-09-17 03:01:27 +0100

    update forms.py
    - add field totsl_rental
    - add Category Form

--- lmsdashboard/lms_app/forms.py
+++ lmsdashboard/lms_app/forms.py
@@ -1,6 +1,14 @@
 from django import forms
 from .models import *
 
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'})
+        }
+
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
@@ -12,8 +20,9 @@ class BookForm(forms.ModelForm):
             'author_img':forms.FileInput(attrs={'class':'form-control'}),
             'pages':forms.NumberInput(attrs={'class':'form-control'}),
             'price':forms.NumberInput(attrs={'class':'form-control'}),
-            'rental_bookprice':forms.NumberInput(attrs={'class':'form-control'}),
-            'retal_period':forms.NumberInput(attrs={'class':'form-control'}),
+            'rental_bookprice':forms.NumberInput(attrs={'class':'form-control', 'id':'rentalprice'}),
+            'retal_period':forms.NumberInput(attrs={'class':'form-control', 'id':'rentaldays'}),
+            'total_rental':forms.NumberInput(attrs={'class':'form-control', 'id':'totalrental'}),
             'status':forms.Select(attrs={'class':'form-control'}),
             'category':forms.Select(attrs={'class':'form-control'}),
         }
